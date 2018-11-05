95ade89c0d831d824e4a0518a721564c5571d364
timkim <timk@adobe.com>
2018-11-05 14:58:51 -0800

    Dumping these files in

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  var app = {
+
+  };
+
+  // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+})();
